681ed9c0f4b4144544d518533109166395c17b4d
SelyBarguest <sourima@hotmail.it>
2018-11-21 16:59:49 +0100

    added device filtering [Fixes #5]

---
+++ core/examples/device_filtering/Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace device_filtering
+{
+    class Test
+    {
+        private static int size = 4;
+
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<Packet>[] list = new List<Packet>[size];
+            string chars = "ABCDEFGHIJK";
+
+            for (int i=0; i<size; i++){
+                list[i] = new List<Packet>();
+            }
+
+            for (int i=0; i<size; i++) {
+                int packet_size = 10;
+                for (int j = 0; j < packet_size; j++) {
+                    string MAC;
+                    do {
+                        MAC = Convert.ToString(chars[rnd.Next(0, chars.Length)]);
+                    } while (list[i].Exists(x => x.getMAC().Equals(MAC.ToUpper())));
+                    list[i].Add(new Packet(MAC, "", DateTime.Now, "", 0)) ;
+                }
+            }
+
+            Console.WriteLine("Testing:");
+            for (int i=0; i<list.Length; i++) {
+                Console.WriteLine("\nFrom ESP32 n " + i);
+                foreach (Packet packet in list[i]) {
+                    Console.WriteLine(packet.ToString());
+                }       
+            }
+            Filterer filterer = new Filterer(list);
+
+            Console.WriteLine("\n Number of devices: " + filterer.numberDevices());
+            Console.ReadKey();
+        }
+    }
+}
