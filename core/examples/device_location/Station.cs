8e369308299a28b215a871b4e3f5d35e403854eb
SelyBarguest <sourima@hotmail.it>
2019-02-16 16:22:32 +0100

    device location and tracker implemented

---
+++ core/examples/device_location/Station.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace device_location {
+    public class Station {
+        readonly int id;
+        double x;
+        double y;
+
+        public Station(int id, double x, double y) {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetPosition(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetId() => id;
+        public double GetX() => x;
+        public double GetY() => y;
+    }
+}
