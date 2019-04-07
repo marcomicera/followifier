cab65fd148b362af93df9b0b9776f871c0f50509
SelyBarguest <sourima@hotmail.it>
2019-04-07 19:41:39 +0200

    Creation of configuration file. Check if configuration file is already present. Overwrite previous configuration.

---
+++ core/examples/configuration/ConfigurationScreen.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Localizer {
+    partial class ConfigurationScreen {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent() {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Confirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(29, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 0, 21, 0);
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 270);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // Confirm
+            // 
+            this.Confirm.Location = new System.Drawing.Point(156, 309);
+            this.Confirm.Name = "Confirm";
+            this.Confirm.Size = new System.Drawing.Size(75, 23);
+            this.Confirm.TabIndex = 4;
+            this.Confirm.Text = "Confirm";
+            this.Confirm.UseVisualStyleBackColor = true;
+            this.Confirm.Click += new System.EventHandler(this.confirm_Click);
+            // 
+            // ConfigurationScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(384, 344);
+            this.Controls.Add(this.Confirm);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ConfigurationScreen";
+            this.Text = "Configuration";
+            this.Load += new System.EventHandler(this.ConfigurationScreen_Load);
+            this.ResumeLayout(false);
+
+        }
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Confirm;
+    }
+}
---
+++ core/examples/configuration/Localizer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Localizer {
+    public partial class Localizer : Form {
+        List<String> stations = new List<String>();
+        Configuration configFile;
+
+        public Localizer(Configuration configFile) {
+            this.configFile = configFile;
+            var settings = configFile.AppSettings.Settings;
+            foreach(var key in settings.AllKeys) {
+                if (key.Contains("station_")) {
+                    stations.Add(settings[key].Value);
+                }
+            }
+            foreach (var key in settings.AllKeys) {
+                System.Diagnostics.Debug.WriteLine("Key: {0} Value: {1}", key, settings[key].Value);
+            }
+            InitializeComponent();
+        }
+
+        private void resetSettings_Click(object sender, EventArgs e) {
+            ConfigurationScreen configurationDialog = new ConfigurationScreen(configFile);
+            configurationDialog.ShowDialog();
+            foreach (var key in configFile.AppSettings.Settings.AllKeys) {
+                System.Diagnostics.Debug.WriteLine("Key: {0} Value: {1}", key, configFile.AppSettings.Settings[key].Value);
+            }
+        }
+    }
+}
