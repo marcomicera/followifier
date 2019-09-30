c4b5af1a88a2822fefd4cee88859a9a6109b1770
Simona <sourima@hotmail.it>
2019-09-30 04:59:59 +0200

    Changes to packet filters logic

--- core/server/src/receiver.cpp
+++ core/server/src/receiver.cpp
@@ -5,15 +5,13 @@ using std::cout;
 using std::endl;
 
 std::mutex receiver::m;
-std::map<std::string, std::vector<followifier::Batch>> receiver::batches;
+std::map<std::string, followifier::Batch> receiver::batches;
 
-bool receiver::isContained(const std::string &mac, std::string hash) {
-    for (const followifier::Batch &batch : batches.at(mac)) {
-        for (int i = 0; i < batch.messages_size(); i++) {
-            const followifier::ESP32Message &msg = batch.messages(i);
-            if (msg.frame_hash() == hash) {
-                return true;
-            }
+bool receiver::isContained(const std::string &mac, const std::string& hash) {
+    for (int i = 0; i < batches.at(mac).messages_size(); i++) {
+        const followifier::ESP32Message &msg = batches.at(mac).messages(i);
+        if (msg.frame_hash() == hash) {
+            return true;
         }
     }
     return false;
@@ -25,18 +23,11 @@ void receiver::addBatch(const followifier::Batch &batch) {
     cout << "received batch from " + mac + " with size " + std::to_string(batch.messages_size()) << endl;
     if (batches.find(mac) == batches.end()) {
         //first time inserting a batch for this mac
-        std::vector<followifier::Batch> list;
-        followifier::Batch empty;
-        for (int i = 0; i <= 3; i++) {
-            //store batch from 4 timeslots
-            list.push_back(empty);
-        }
-        list.push_back(batch);
-        batches.insert(std::pair<std::string, std::vector<followifier::Batch>>(mac, list));
+        batches.insert(std::pair<std::string, followifier::Batch>(mac, batch));
     } else {
         //new timeslot
-        batches.at(mac).erase(batches.at(mac).begin(), batches.at(mac).begin());
-        batches.at(mac).push_back(batch);
+        batches.clear();
+        batches.insert(std::pair<std::string, followifier::Batch>(mac, batch));
     }
 
     //check batches
@@ -53,28 +44,8 @@ void receiver::addBatch(const followifier::Batch &batch) {
                 //add to db
                 cout << "added to db message with hash " << message.frame_hash() << endl;
                 //remove from stored batches
-                removeMessage(message.frame_hash());
             }
         }
     }
     m.unlock();
 }
-
-void receiver::removeMessage(const std::string &hash) {
-    for (auto &pairs : batches) {
-        removeMessageFromList(hash, pairs.second);
-    }
-}
-
-void receiver::removeMessageFromList(const std::string &hash, std::vector<followifier::Batch> values) {
-    for (auto &list : values) {
-        for (int i = 0; i < list.messages_size(); i++) {
-            if (list.messages(i).frame_hash() == hash) {
-                list.mutable_messages()->DeleteSubrange(i, 1);
-                return;
-            }
-        }
-    }
-}
-
-
--- core/server/src/receiver.h
+++ core/server/src/receiver.h
@@ -13,11 +13,9 @@ class receiver {
 public:
     static void addBatch(const followifier::Batch& batch);
 private:
-    static bool isContained(const std::string& mac, std::string hash);
+    static bool isContained(const std::string& mac, const std::string& hash);
     static std::mutex m;
-    static std::map<std::string, std::vector<followifier::Batch>> batches;
-    static void removeMessage(const std::string& hash);
-    static void removeMessageFromList(const std::string& hash, std::vector<followifier::Batch> values);
+    static std::map<std::string, followifier::Batch> batches;
  };
 
 #endif //CORE_RECEIVER_H
