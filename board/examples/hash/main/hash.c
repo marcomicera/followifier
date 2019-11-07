85b2806b29eb1059142d96530c7b06c41856fb72
Marco Micera <marco.micera@gmail.com>
2019-11-07 01:34:00 +0100

    Board's hash example completed

--- board/examples/hash/main/hash.c
+++ board/examples/hash/main/hash.c
@@ -6,6 +6,9 @@
 #include "stddef.h"
 #include <string.h>
 #include <stdio.h>
+#include <esp_log.h>
+
+static const char* TAG = "hash-example";
 
 /**
  * All-purpose hash function.
@@ -14,8 +17,7 @@
  * @param payload   payload to be hashed.
  * @return          the digest.
  */
-size_t* hash(const char *payload) {
-    size_t shaResult[32];
+void hash(const char *payload, unsigned char* result) {
     mbedtls_md_context_t ctx;
     mbedtls_md_type_t md_type = MBEDTLS_MD_SHA256;
     const size_t payloadLength = strlen(payload);
@@ -23,21 +25,20 @@ size_t* hash(const char *payload) {
     mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(md_type), 0);
     mbedtls_md_starts(&ctx);
     mbedtls_md_update(&ctx, (const unsigned char *) payload, payloadLength);
-    mbedtls_md_finish(&ctx, shaResult);
+    mbedtls_md_finish(&ctx, result);
     mbedtls_md_free(&ctx);
-    return shaResult;
 }
 
-int main() {
-
+void app_main(void) {
     char *payload = "Hello SHA 256!";
-    size_t* result = hash(payload);
+    unsigned char result[32];
+    ESP_LOGI(TAG, "Hashing the following sentence: %s", payload);
+    hash(payload, result);
+    ESP_LOGI(TAG, "Printing digest...");
     for(unsigned long i= 0; i < sizeof(result); i++){
         char str[3];
         sprintf(str, "%02x", (int)result[i]);
-        printf("%s", str);
+        ESP_LOGI(TAG, "%s", str);
     }
-
-    return 0;
 }
 
