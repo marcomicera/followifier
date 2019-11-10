58e4387f0a5ed4328f01204c5b6597e5afa27039
Marco Micera <marco.micera@gmail.com>
2019-11-10 16:40:50 +0100

    Error management in asynchronous read handler

--- core/server/src/connection.cpp
+++ core/server/src/connection.cpp
@@ -20,51 +20,58 @@ tcp::socket &connection::socket() {
 
 void connection::start() {
     try {
-        cout << "new connection established.\nReady to receive a new batch... " << std::flush;
+        cout << "new connection established. Reading batch asynchronously... " << endl;
         GOOGLE_PROTOBUF_VERIFY_VERSION;
         boost::asio::async_read_until(socket_, buf, delimiter,
                                       boost::bind(&connection::handle_read, shared_from_this(),
                                                   boost::asio::placeholders::error,
                                                   boost::asio::placeholders::bytes_transferred));
-        //        std::string data{
-//                boost::asio::buffers_begin(buf.data()),
-//                boost::asio::buffers_begin(buf.data()) + buf.size() - delimiter.size()
-//        };
-//        data = data.substr(0, data.size() - 1);
-//        if (!batch.ParseFromString(data)) {
-//            cerr << "failed to parse batch (length" << data.length() << ")." << endl; // intentionally lowercase
-//            return;
-//        }
-//        receiver::addBatch(batch, database);
     } catch (const std::exception &e) {
         cerr << "failed to parse batch." << endl; // intentionally lowercase
     }
 }
 
+/**
+ * Handles the asynchronous reception of data from a board.
+ *
+ * @param error result of the operation.
+ * @param bytes_transferred the number of bytes in the `streambuf`'s get area up to including the delimiter. 0 in an error occurred.
+ */
 void connection::handle_read(const boost::system::error_code &error,
                              size_t bytes_transferred) {
-    followifier::Batch batch;
-    database database;
-    // Verify buf contains more data beyond the delimiter. (e.g.
-    // async_read_until read beyond the delimiter)
-    cout << buf.data().size() << endl;
-    cout << bytes_transferred << endl;
-    // Extract up to the first delimiter.
-    std::string data{
-            buffers_begin(buf.data()),
-            buffers_begin(buf.data()) + bytes_transferred - delimiter.size()
-    };
 
-    // Consume through the first delimiter so that subsequent async_read_until
-    // will not reiterate over the same data.
-    buf.consume(bytes_transferred);
-
-    if (!batch.ParseFromString(data)) {
-        cerr << "failed to parse batch (length" << data.length() << ")." << endl; // intentionally lowercase
-        return;
+    // If the batch has not been received correctly
+    if (!error || bytes_transferred == 0) {
+        cerr << "Something went wrong while receiving a batch." << endl << "Error: " << error.message() << endl
+             << "Number of transferred bytes: " << bytes_transferred << endl;
     }
-    receiver::addBatch(batch, database);
+    else {
 
+        followifier::Batch batch;
+        database database;
+
+        // Verify buf contains more data beyond the delimiter. (e.g.
+        // async_read_until read beyond the delimiter)
+        assert(buf.size() > bytes_transferred);
+
+        // Extract up to the first delimiter.
+        cout << endl << "Buffer size: " << buf.size() << ", transferred bytes: " << bytes_transferred
+             << ", delimiter size: " << delimiter.size() << endl;
+        std::string data{
+                buffers_begin(buf.data()),
+                buffers_begin(buf.data()) + bytes_transferred - delimiter.size()
+        };
+
+        // Consume through the first delimiter so that subsequent async_read_until
+        // will not reiterate over the same data.
+        buf.consume(bytes_transferred);
+
+        if (!batch.ParseFromString(data)) {
+            cerr << "failed to parse batch (length" << data.length() << ")." << endl; // intentionally lowercase
+            return;
+        }
+        receiver::addBatch(batch, database);
+    }
 }
 
 connection::connection(boost::asio::io_service &io_service) : socket_(io_service) {}
--- core/server/src/connection.h
+++ core/server/src/connection.h
@@ -9,7 +9,9 @@ using boost::asio::ip::tcp;
 #define CORE_CONNECTION_H
 
 class connection : public boost::enable_shared_from_this<connection> {
+
 public:
+
     typedef boost::shared_ptr<connection> pointer;
 
     static pointer create(boost::asio::io_service &io_service);
@@ -17,15 +19,20 @@ public:
     tcp::socket &socket();
 
     void start();
-    void handle_read(const boost::system::error_code&,size_t);
+
+    void handle_read(const boost::system::error_code &, size_t);
 
 private:
-    connection(boost::asio::io_service& io_service);
-    boost::asio::streambuf buf;
-    tcp::socket socket_;
-    std::string message_;
-    const std::string delimiter = "\n\r\n\r";
 
+    connection(boost::asio::io_service &io_service);
+
+    boost::asio::streambuf buf;
+
+    tcp::socket socket_;
+
+    std::string message_;
+
+    const std::string delimiter = "\n\r\n\r";
 };
 
 #endif //CORE_CONNECTION_H
