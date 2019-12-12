f7eeea1552a114db1060827496f5be3796dc2463
Marco Micera <marco.micera@gmail.com>
2019-12-12 14:26:20 +0100

    Replayer (#69) saves/retrieves trace in custom dir
    Useful for #71

--- core/examples/packet_replayer/replayer.py
+++ core/examples/packet_replayer/replayer.py
@@ -1,10 +1,10 @@
 import argparse
-import pprint
-import threading
-import socket
 import os
-import sys
+import pprint
+import socket
+import threading
 import time
+from datetime import datetime
 
 
 def replay(filename, port):
@@ -14,20 +14,24 @@ def replay(filename, port):
         data = f.read()
     s.send(data)
 
-def service_replay(port, prefix, batch_number):
+
+def service_replay(port, prefix, batch_number, trace_dir):
     threads = []
     for batch_index in range(batch_number):
         for board_num in range(3):
-            filename = "{}_b{}_t{}".format(prefix, board_num, batch_index + 1)
-            print(f"Checking whether file {filename} exists")
+            filename = "{}/{}b{}_t{}".format(trace_dir, prefix, board_num, batch_index + 1)
             if os.path.exists(filename):
                 print("Sending {} to server.".format(filename))
                 t = threading.Thread(target=replay, args=(filename, port))
                 t.start()
                 threads.append(t)
+            else:
+                print("File {} does not exist: please make sure you specify the trace directory "
+                      "with the --trace-dir flag.".format(filename))
         time.sleep(60)
     for t in threads:
         t.join()
+
 
 def capture(socket, filename):
     message = b""
@@ -42,9 +46,9 @@ def capture(socket, filename):
         f.write(message)
 
 
-def service_capture(port, prefix, batch_number):
+def service_capture(port, prefix, batch_number, trace_dir):
     board_counter = 0
-    counters = {'dummy': 0} # just to init, for the loop
+    counters = {'dummy': 0}  # just to init, for the loop
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
@@ -54,9 +58,10 @@ def service_capture(port, prefix, batch_number):
         exit(1)
 
     s.listen(3)
-    print("Socket listening.") 
+    print("Socket listening.")
     threads = []
-    while not all(counter >= batch_number for counter in counters.values()): 
+    os.mkdir(trace_dir)
+    while not all(counter >= batch_number for counter in counters.values()):
         conn, addr = s.accept()
         addr = addr[0]
         # Using addr as a unique fingerprint here.
@@ -66,7 +71,7 @@ def service_capture(port, prefix, batch_number):
         else:
             counters[addr][1] += 1
         pprint.pprint(counters)
-        filename = "{}_b{}_t{}".format(prefix, *counters[addr])
+        filename = "{}/{}b{}_t{}".format(trace_dir, prefix, *counters[addr])
         print("Capturing {} batch".format(filename))
         t = threading.Thread(target=capture, args=(conn, filename))
         t.start()
@@ -77,19 +82,20 @@ def service_capture(port, prefix, batch_number):
     print("Received following number of batches from these ips:")
     pprint.pprint(counters)
 
+
 modes = {
-        'replay': service_replay,
-        'capture': service_capture
-        }
+    'replay': service_replay,
+    'capture': service_capture
+}
 
 parser = argparse.ArgumentParser()
-parser.add_argument('mode', choices=['capture', 'replay'])
-parser.add_argument('port', type=int, help="port to listen on in capture mode, or to send packets in replay mode.")
+parser.add_argument('mode', choices=['capture', 'replay'], help='')
+parser.add_argument('port', type=int, help="Port to listen on in capture mode, or to send packets in replay mode.")
 parser.add_argument('--batch-number', type=int, default=10, help="Number of batches to capture or replay.")
-# setting it as default
-# parser.add_argument('--boards-number', type=int, default=3, help="Number of expected sockets")
-parser.add_argument('--file-prefix', type=str, default="batch_", help="prefix for saved files.")
+parser.add_argument('--file-prefix', type=str, default="batch_", help="Prefix for saved files.")
+parser.add_argument('--trace-dir', type=str, default=datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
+                    help="Where to save/retrieve batches.")
 args = parser.parse_args()
 
 print("Activating {} mode...".format(args.mode))
-modes[args.mode](args.port, args.file_prefix, args.batch_number)
+modes[args.mode](args.port, args.file_prefix, args.batch_number, args.trace_dir)
