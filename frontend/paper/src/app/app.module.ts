6a033c784ef7e3923fe23cefb339ff5ff46fe3f0
Marco Micera <marco.micera@gmail.com>
2020-01-05 18:55:21 +0100

    Paper theme upload (#86)

---
+++ frontend/paper/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from "ngx-toastr";
+
+import { SidebarModule } from './sidebar/sidebar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule} from './shared/navbar/navbar.module';
+import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
+
+import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes,{
+      useHash: true
+    }),
+    SidebarModule,
+    NavbarModule,
+    ToastrModule.forRoot(),
+    FooterModule,
+    FixedPluginModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
