84f9fad0d3d53ad794b69a73a5c336a033793110
Marco Micera <marco.micera@gmail.com>
2020-01-06 15:21:51 +0100

    Radar

--- frontend/paper/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/paper/src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js';
+import {Component, OnInit} from '@angular/core';
+import Chart, {ChartDataSets, ChartPoint, ChartTooltipItem} from 'chart.js';
 import {interval, Subscription} from 'rxjs';
 import {ApiService} from '../../service/api/api.service';
 import {ActivatedRoute} from "@angular/router";
+
 // import {Label} from "ng2-charts";
 
 
@@ -12,212 +13,233 @@ import {ActivatedRoute} from "@angular/router";
     templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
-  public canvas : any;
-  public ctx;
-  public chartColor;
-  public chartEmail;
-  public chartHours;
+    public canvas: any;
+    public ctx;
+    public emailChart;
+    public numberOfDevices;
+    public radar;
+    private updateSubscription: Subscription;
 
-  numberDevice: number;
-  // lineChartLabels: Label[] = [];
-  private updateSubscription: Subscription;
-  private static readonly lineChartUpdatingFrequency = 5 * 1000;  // low value for testing purposes
-  private static readonly lineChartDataPoints = 5;
-  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
+    constructor(private route: ActivatedRoute, private apiService: ApiService) {
+    }
 
-    ngOnInit(){
-      this.chartColor = "#FFFFFF";
+    // Number of devices
+    totalNumberOfDevices: number;
+    private static readonly numberOfDevicesUpdatingFrequency = 5 * 1000;  // low value for testing purposes
+    private static readonly maxNumberOfDevicesDataPoints = 5;
 
-      this.canvas = document.getElementById("chartHours");
-      this.ctx = this.canvas.getContext("2d");
+    // Radar
+    private static readonly radarUpdatingFrequency = 2 * 1000; // low value for testing purposes
+    private radarToolTipItem: ChartTooltipItem[] = [];
+    private radarDataSet: ChartDataSets[] = [{
+        label: 'Boards',
+        pointRadius: 10,
+        pointBackgroundColor: '#51bcd9',
+        backgroundColor: '#51bcd9',
+        hoverBackgroundColor: '#51bcd9',
+        data: [],
+        hideInLegendAndTooltip: false,
+    },
+        {
+            label: 'Devices',
+            pointRadius: 10,
+            pointBackgroundColor: '#fbc658',
+            backgroundColor: '#fbc658',
+            hoverBackgroundColor: '#fbc658',
+            data: []
+        }];
 
-      this.chartHours = new Chart(this.ctx, {
-        type: 'line',
+    ngOnInit() {
 
-        data: {
-          labels: ["5 mins ago", "4 mins ago", "3 mins ago", "2 mins ago", "1 min ago"],
-          datasets: [{
-              label: 'Devices',
-              borderColor: "#6bd098",
-              backgroundColor: "#6bd098",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: []
+        // Number of devices
+        this.canvas = document.getElementById("chartHours");
+        this.ctx = this.canvas.getContext("2d");
+        this.numberOfDevices = new Chart(this.ctx, {
+            type: 'line',
+            data: {
+                labels: ["5 mins ago", "4 mins ago", "3 mins ago", "2 mins ago", "1 min ago"],
+                datasets: [{
+                    label: 'Devices',
+                    borderColor: "#6bd098",
+                    backgroundColor: "#6bd098",
+                    pointRadius: 0,
+                    pointHoverRadius: 0,
+                    borderWidth: 3,
+                    data: []
+                }
+                ]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    enabled: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            fontColor: "#9f9f9f",
+                            beginAtZero: false,
+                            maxTicksLimit: 5,
+                            //padding: 20
+                        },
+                        gridLines: {
+                            drawBorder: false,
+                            zeroLineColor: "#ccc",
+                            color: 'rgba(255,255,255,0.05)'
+                        }
+                    }],
+                    xAxes: [{
+                        barPercentage: 1.6,
+                        gridLines: {
+                            drawBorder: false,
+                            color: 'rgba(255,255,255,0.1)',
+                            zeroLineColor: "transparent",
+                            display: false,
+                        },
+                        ticks: {
+                            padding: 20,
+                            fontColor: "#9f9f9f"
+                        }
+                    }]
+                },
             }
-          ]
-        },
-        options: {
-          responsive: true,
-
-          legend: {
-            display: false
-          },
-
-          tooltips: {
-            enabled: false
-          },
-
-          scales: {
-            yAxes: [{
-
-              ticks: {
-                fontColor: "#9f9f9f",
-                beginAtZero: false,
-                maxTicksLimit: 5,
-                //padding: 20
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "#ccc",
-                color: 'rgba(255,255,255,0.05)'
-              }
-
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent",
-                display: false,
-              },
-              ticks: {
-                padding: 20,
-                fontColor: "#9f9f9f"
-              }
-            }]
-          },
-        }
-      });
+        });
+        this.updateSubscription = interval(DashboardComponent.numberOfDevicesUpdatingFrequency).subscribe((val) => {
+            console.log('Updating devices number line chart');
+            this.apiService.getDevicesNumber().subscribe(data => {
+                    this.totalNumberOfDevices = +data;
+                    this.numberOfDevices.data.datasets[0].data.push(this.totalNumberOfDevices);
+                    // this.lineChartLabels.push(String(new Date().getMinutes()));
+                    if (this.numberOfDevices.data.datasets[0].data.length > DashboardComponent.maxNumberOfDevicesDataPoints) {
+                        this.numberOfDevices.data.datasets[0].data.shift();
+                        // this.lineChartLabels.shift();
+                    }
+                    this.numberOfDevices.update();
+                }
+            );
+        });
 
 
-      this.updateSubscription = interval(DashboardComponent.lineChartUpdatingFrequency).subscribe((val) => {
-        console.log('Updating devices number line chart');
-        this.apiService.getDevicesNumber().subscribe(data => {
-              this.numberDevice = +data;
-              this.chartHours.data.datasets[0].data.push(this.numberDevice);
-              // this.lineChartLabels.push(String(new Date().getMinutes()));
-              if (this.chartHours.data.datasets[0].data.length > DashboardComponent.lineChartDataPoints) {
-                this.chartHours.data.datasets[0].data.shift();
-                // this.lineChartLabels.shift();
-              }
-              this.chartHours.update();
+        // Email statistics
+        this.canvas = document.getElementById("chartEmail");
+        this.ctx = this.canvas.getContext("2d");
+        this.emailChart = new Chart(this.ctx, {
+            type: 'pie',
+            data: {
+                labels: [1, 2, 3],
+                datasets: [{
+                    label: "Emails",
+                    pointRadius: 0,
+                    pointHoverRadius: 0,
+                    backgroundColor: [
+                        '#e3e3e3',
+                        '#4acccd',
+                        '#fcc468',
+                        '#ef8157'
+                    ],
+                    borderWidth: 0,
+                    data: [342, 480, 530, 120]
+                }]
+            },
+
+            options: {
+
+                legend: {
+                    display: false
+                },
+
+                pieceLabel: {
+                    render: 'percentage',
+                    fontColor: ['white'],
+                    precision: 2
+                },
+
+                tooltips: {
+                    enabled: false
+                },
+
+                scales: {
+                    yAxes: [{
+
+                        ticks: {
+                            display: false
+                        },
+                        gridLines: {
+                            drawBorder: false,
+                            zeroLineColor: "transparent",
+                            color: 'rgba(255,255,255,0.05)'
+                        }
+
+                    }],
+
+                    xAxes: [{
+                        barPercentage: 1.6,
+                        gridLines: {
+                            drawBorder: false,
+                            color: 'rgba(255,255,255,0.1)',
+                            zeroLineColor: "transparent"
+                        },
+                        ticks: {
+                            display: false,
+                        }
+                    }]
+                },
             }
-        );
-      });
+        });
 
 
-      this.canvas = document.getElementById("chartEmail");
-      this.ctx = this.canvas.getContext("2d");
-      this.chartEmail = new Chart(this.ctx, {
-        type: 'pie',
-        data: {
-          labels: [1, 2, 3],
-          datasets: [{
-            label: "Emails",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#e3e3e3',
-              '#4acccd',
-              '#fcc468',
-              '#ef8157'
-            ],
-            borderWidth: 0,
-            data: [342, 480, 530, 120]
-          }]
-        },
+        // Radar
+        var radarCanvas = document.getElementById("speedChart");
+        this.radar = new Chart(radarCanvas, {
+            type: 'scatter',
+            hover: false,
+            data: {
+                labels: [],
+                datasets: this.radarDataSet
+            },
+            options: {
+                legend: {
+                    display: false,
+                    position: 'top'
+                }
+            }
+        });
+        this.updateSubscription = interval(DashboardComponent.radarUpdatingFrequency).subscribe((val) => {
+            console.log('Updating radar');
+            this.radarToolTipItem = [];
+            this.apiService.getBoards().subscribe(data => {
+                this.radarDataSet[0].data = [];
 
-        options: {
+                data.forEach(d => {
+                    console.log('board: ' + d.mac);
 
-          legend: {
-            display: false
-          },
+                    (this.radarDataSet[0].data as ChartPoint[]).push({x: d.x, y: d.y, t: d.mac});
+                    (this.radarToolTipItem.push({
+                        label: d.mac,
+                        datasetIndex: 0,
+                        index: this.radarDataSet[0].data.length
+                    }));
 
-          pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
-          },
-
-          tooltips: {
-            enabled: false
-          },
-
-          scales: {
-            yAxes: [{
-
-              ticks: {
-                display: false
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "transparent",
-                color: 'rgba(255,255,255,0.05)'
-              }
-
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent"
-              },
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-        }
-      });
-
-      var speedCanvas = document.getElementById("speedChart");
-
-      var dataFirst = {
-        data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
-        fill: false,
-        borderColor: '#fbc658',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#fbc658',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8,
-      };
-
-      var dataSecond = {
-        data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
-        fill: false,
-        borderColor: '#51CACF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#51CACF',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
-
-      var speedData = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [dataFirst, dataSecond]
-      };
-
-      var chartOptions = {
-        legend: {
-          display: false,
-          position: 'top'
-        }
-      };
-
-      var lineChart = new Chart(speedCanvas, {
-        type: 'line',
-        hover: false,
-        data: speedData,
-        options: chartOptions
-      });
+                });
+            });
+            this.apiService.getDevices().subscribe(data => {
+                this.radarDataSet[1].data = [];
+                data.forEach(d => {
+                    console.log('device: ' + d._id);
+                    (this.radarDataSet[1].data as ChartPoint[]).push({x: d.x, y: d.y, t: d._id});
+                    (this.radarToolTipItem.push({
+                        label: d._id,
+                        datasetIndex: 1,
+                        index: this.radarDataSet[1].data.length
+                    }));
+                });
+            });
+            this.radar.update();
+        });
     }
 }
